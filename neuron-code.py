6e7f8954d3c1756bec4505772bb7730cf5195dee
Parth <upadhyaparth420@icloud.com>
2023-05-19 09:18:31 +0530

    Code for 3 neurons

--- neuron-code.py
+++ neuron-code.py
@@ -1,10 +1,30 @@
 input_list = [1, 2, 3, 2.5]
-associated_weights = [0.2, 0.8, -0.5, 1]
-bias = 2
+associated_weights1 = [0.2, 0.8, -0.5, 1]
+associated_weights2 = [0.5, -0.91, 0.26, -0.5]
+associated_weights3 = [-0.26, -0.27, 0.17, 0.87]
+bias1 = 2
+bias2 = 3
+bias3 = 0.5
 
 
-output = (input_list[0] * associated_weights[0] + 
-        input_list[1] * associated_weights[1] + 
-        input_list[2] * associated_weights[2] +
-        bias)
+output = [
+        # Neuron 1
+        (input_list[0] * associated_weights1[0] + 
+        input_list[1] * associated_weights1[1] + 
+        input_list[2] * associated_weights1[2] +
+        input_list[3] * associated_weights1[3] +
+        bias1),
+        # Neuron 2
+        (input_list[0] * associated_weights2[0] + 
+        input_list[1] * associated_weights2[1] + 
+        input_list[2] * associated_weights2[2] +
+        input_list[3] * associated_weights2[3] +
+        bias2),
+        # Neuron 3
+        (input_list[0] * associated_weights3[0] + 
+        input_list[1] * associated_weights3[1] + 
+        input_list[2] * associated_weights3[2] +
+        input_list[3] * associated_weights3[3] +
+        bias3)
+        ]
 print(output)
